bcc3f76ed7ab6273e7cb446724cdc8dd34fe6527
Ben Lubar <ben.lubar@gmail.com>
2017-05-21 16:04:17 -0500

    Use labormanager instead of autolabor.

--- ai.cpp
+++ ai.cpp
@@ -281,7 +281,7 @@ command_result AI::startup(color_ostream & out)
 {
     command_result res = CR_OK;
     if (res == CR_OK && config.manage_labors)
-        res = Core::getInstance().runCommand(out, "enable autolabor");
+        res = Core::getInstance().runCommand(out, "enable labormanager");
     if (res == CR_OK)
         res = pop->startup(out);
     if (res == CR_OK)
