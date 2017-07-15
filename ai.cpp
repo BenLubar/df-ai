9e4d2ce035528261ed91b20170ea0dfcbbe7f631
Ben Lubar <ben.lubar@gmail.com>
2017-07-15 11:28:41 -0500

    Disable confirm at startup. Closes #28.

--- ai.cpp
+++ ai.cpp
@@ -393,7 +393,7 @@ void AI::event(const std::string & name, const Json::Value & payload)
 
 command_result AI::startup(color_ostream & out)
 {
-    command_result res = CR_OK;
+    command_result res = Core::getInstance().runCommand(out, "disable confirm");;
     if (res == CR_OK && config.manage_labors)
         res = Core::getInstance().runCommand(out, "enable autolabor");
     if (res == CR_OK)
